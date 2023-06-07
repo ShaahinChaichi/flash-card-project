9270b8f41142e05c12ac29ec63472c7d15ca85c5
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-07 19:29:08 +0330

    Buttons added

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image)
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.config(highlightthickness=0)
 known_button.grid(row=1, column=1)
 
 window.mainloop()
